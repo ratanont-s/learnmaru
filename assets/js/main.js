1d0252250feb0587b1225761384eab4a8f821639
rathanont <rathanont.s@gmail.com>
2020-02-28 01:30:50 +0700

    update

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+window.onscroll = function () {
+    headerSticky()
+};
+
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function headerSticky() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+$(function () {
+    'use strict'
+
+    $('[data-toggle="offcanvas"]').on('click', function () {
+        $('.offcanvas-collapse').toggleClass('open')
+    })
+})
